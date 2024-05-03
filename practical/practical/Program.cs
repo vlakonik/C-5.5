9c8dbe5f9ac3abb5cdae18d23ec51dac07ef5fd2
vlakonik <wlakonik@gmail.com>
2024-05-03 19:06:06 +0300

    :cookie: :cookie:

--- practical/practical/Program.cs
+++ practical/practical/Program.cs
@@ -23,7 +23,10 @@ namespace practical
             Pause();
         }
 
-
+        /// <summary>
+        /// 1.0) Мой первоначальный метод получения текста.
+        /// </summary>
+        /// <returns></returns>
         private static string[] GetText()
         {
             Console.Write("Введите текст: ");
@@ -32,7 +35,11 @@ namespace practical
 
             Console.WriteLine($"\nПервоначальный текст: {text}");
             return array;
-        }// 1.0) Мой первоначальный метод получения текста.
+        }
+        /// <summary>
+        /// 1.1) Мой первоначальный метод разделения текста.
+        /// </summary>
+        /// <param name="array"></param>
         private static void SplitText(string [] array)
         {
             Console.WriteLine("\nРазделенный текст: ");
@@ -40,23 +47,32 @@ namespace practical
             {
                 Console.WriteLine($"\n{i}");
             }
-        }// 1.1) Мой первоначальный метод разделения текста.
-
-
-        private static string GettingText()// 2.0) метод получения текста вроде как по заданию
+        }
+        /// <summary>
+        /// 2.0) метод получения текста вроде как по заданию
+        /// </summary>
+        /// <returns></returns>
+        private static string GettingText()
         {
             Console.Write("Введите текст: ");
             string text = Console.ReadLine();
             return text;
         }
-        private static string[] SplittingText(string text) // 2.1) метод разделения текста вроде как по заданию
+        /// <summary>
+        /// 2.1) метод разделения текста вроде как по заданию
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        private static string[] SplittingText(string text)
         {
             Console.WriteLine("\nРазделенный текст: ");
             string[] array = text.Split(' ');
             return array;
         }
-
-        private static void GetAndSplitText() // 3.0) решение одним методом, сделал ради интереса
+        /// <summary>
+        /// 3.0) решение одним методом, сделал ради интереса
+        /// </summary>
+        private static void GetAndSplitText() 
         {
             Console.Write("Введите текст: ");
             string text = Console.ReadLine();
@@ -70,10 +86,12 @@ namespace practical
                 Console.WriteLine($"\n{i}");
             }
         }
-
+        /// <summary>
+        /// пауза
+        /// </summary>
         private static void Pause()
         {
             Console.ReadKey();
-        }// пауза
+        }
     }
 }
